42e567ae1ca5f7e6fbcbd9aafe878714abb3457d
Walter Ye <hupeng.net@hotmail.com>
2025-03-29 12:56:28 +0000

    update go version

--- diy-script.sh
+++ diy-script.sh
@@ -11,7 +11,7 @@
 
 # 移除要替换的包
 rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
+# rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
